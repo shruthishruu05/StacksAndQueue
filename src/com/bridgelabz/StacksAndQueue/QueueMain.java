bb3f7a29079e6cac4a1fd7599cbd667e2bfaed8b
shruthishruu05 <shruthi.m@perfios.com>
2021-09-01 00:38:49 +0530

    ADDED: Demonstration of enqueue operation

---
+++ src/com/bridgelabz/StacksAndQueue/QueueMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.StacksAndQueue;
+
+import com.bridgelabz.linkedlist.MyNode;
+
+public class QueueMain {
+	public static void main(String[] args) {
+		MyQueue<Integer> myQueue = new MyQueue<Integer>();
+		
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+		myQueue.enqueue(firstNode);
+		myQueue.enqueue(secondNode);
+		myQueue.enqueue(thirdNode);
+		myQueue.printQueue();
+	}
+}
